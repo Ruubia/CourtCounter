7750754be8892f81cab34f015955815ab6eca628
Beatriz <b.carmonaj@gmail.com>
2017-05-30 20:16:28 +0200

    Update MainActivity.java

--- CourtCounter/app/src/main/java/com/example/bea/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/bea/courtcounter/MainActivity.java
@@ -34,71 +34,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
     @Override
 
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+         getMenuInflater().inflate(R.menu.menu_main, menu);
 
         return true;
 
     }
 
 
-
     @Override
 
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        // Handle action bar item clicks here. The action bar will
-
-        // automatically handle clicks on the Home/Up button, so long
-
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        int id = item.getItemId();
-
-
-
-        //noinspection SimplifiableIfStatement
-
-        if (id == R.id.action_settings) {
-
-            return true;
-
-        }
-
-
-
-        return super.onOptionsItemSelected(item);
-
+        int id = item.getItemId();               
+        return id == R.id.action_settings || return super.onOptionsItemSelected(item);
+       
+    
     }
-
-
-
+        
+        
     /**
-
      * Increase the score for Team A by 1 point.
-
      */
 
     public void addOneForTeamA(View v) {
 
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
+            
+   }
 
-    }
 
-
-
-    /**
-
+   /**
      * Increase the score for Team A by 2 points.
-
      */
 
     public void addTwoForTeamA(View v) {
@@ -109,11 +78,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     /**
-
      * Increase the score for Team A by 3 points.
-
      */
 
     public void addThreeForTeamA(View v) {
@@ -122,30 +88,27 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
 
     }
+        
     /**
-
      * Increase the score for Team B by 1 points.
-
-     */
+     */        
     public void addOneForTeamB(View v) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
 
     }
+        
     /**
-
      * Increase the score for Team B by 2 points.
-
      */
     public void addTwoForTeamB(View v) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
 
     }
+        
     /**
-
      * Increase the score for Team B by 3 points.
-
      */
     public void addThreeForTeamB(View v) {
         scoreTeamB = scoreTeamB + 3;
@@ -163,21 +126,16 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamB(scoreTeamB);
     }
 
-
-
+   
     /**
-
      * Displays the given score for Team A.
-
      */
-
     public void displayForTeamA(int score) {
-
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-
         scoreView.setText(String.valueOf(score));
 
     }
+        
     /**
      * Displays the given score for Team B.
      */
